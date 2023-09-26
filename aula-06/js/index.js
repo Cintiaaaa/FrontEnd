3894ad225398663505ad5678c892758993e352fd
Cintiaaaa <cintiadeazevedocastro@yahoo.com.br>
2023-09-25 22:22:13 -0300

    Trabalho individual FrontEnd

---
+++ aula-06/js/index.js
@@ -0,0 +1,12 @@
+//Comentário de linha
+/*Comentário
+ de 
+ bloco */
+//alert("Alert no JS externo");
+let numero1 = parseFloat(prompt("Digite o primeiro número: "));
+
+let numero2 = parseFloat(prompt("Digite o segundo número: "));
+
+let media = (numero1 + numero2) / 2;
+
+alert("A média dos números é: " + media);
